1d9b981f498ed9b9b20d6af838b3e4f248054cfb
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-01-12 07:57:34 +0530

    Add Compose preview dependency to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.3")
     implementation(project(":fancy-switch"))
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.3")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.3.3")
 }
